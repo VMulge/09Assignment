e78c4b055efca4f26f10b51266cb49fd45f21b84
VMulge <mulgevr2530>
2024-04-04 22:53:24 +0530

    first upload on github

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  const [counter, setCounter] = React.useState(0);
+
+  return (
+    <div>
+      <h2>Counter: {counter}</h2>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+    </div>
+  );
+};
+
+export default App
